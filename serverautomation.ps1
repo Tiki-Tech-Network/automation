54f4b0ad379d10c3b965709b0fe256bef4ca17c0
Ian Bennett <subtropicalhorseback@gmail.com>
2023-12-18 13:20:18 -0600

    think i have the PS updater built, pushing to test on server as a downloaded .ps1

--- serverautomation.ps1
+++ serverautomation.ps1
@@ -1,2 +1,32 @@
 # this powershell script will be used to stand up a windows server
-Write-Host 'This Document is from the Internet.'
+# created Dec 18-20, 2023 by Tiki-Tech-Network-Solutions
+
+## step 1, update to PS 7.2.0 from stock PS 5.x
+$url1 = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/PowerShell-7.2.0-win-x64.msi"
+$output1 = "C:\Users\Administrator\Downloads\PowershellUpgrade.msi"
+
+### retrieve the update file
+try {
+    Invoke-WebRequest -Uri $url1 -OutFile $output1 -ErrorAction Stop
+    Write-Host "Download successful.`n`n"
+}
+catch {
+    Write-Host "Error downloading the MSI file: $_`n`n"
+    exit 1
+}
+
+
+### implement the update
+try {
+    Start-Process -Wait -FilePath "msiexec.exe" -ArgumentList "/i $output1 /qn" -ErrorAction Stop
+    Write-Host "PowerShell installation successful.`n`n"
+}
+catch {
+    Write-Host "Error installing PowerShell 7.2.0: $_`n`n"
+    exit 1
+}
+
+### show status
+$PSVer = $PSVersionTable.PSVersion
+Write-Host "PowerShell version after update: $($PSVer.Major).$($PSVer.Minor).$($PSVer.Build)`n`n"
+
