2e2e475d2f60c72091fc2314917871421717accd
Ian Bennett <subtropicalhorseback@gmail.com>
2023-12-20 14:45:41 -0600

    for real - think i fixed the typo. line 406

--- wash.ps1
+++ wash.ps1
@@ -357,6 +357,7 @@ function Server-Maintenance {
 ##################################
 
 function Create-Network-Folders {
+
     # Prompt user for folder name - THIS WORKS
     $folderName = Read-Host "Enter the name of the shared folder"
 
@@ -446,34 +447,43 @@ function Create-Network-Folders {
         return
     }
 
+}
+
+##################################
+
+function Backup {
+
+    # Define the source directories to back up
+
+    #$sourceDirs = @(
 
 
-# Define the source directories to back up
-#$sourceDirs = @(
-#    "C:\Users\Administrator\FINANCE",
-#    "C:\Users\Administrator\HR",
-#    "C:\Users\Administrator\IT",
-#    "C:\Users\Administrator\Research"
-#)
-# Define the root backup directory on the local disk
-#$backupRootDir = "C:\Backups"
-# Create a timestamped directory for this backup
-#$timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
-#$backupDir = Join-Path $backupRootDir ("Backup_" + $timestamp)
-#New-Item -Path $backupDir -ItemType Directory
-# Copy each source directory to the timestamped backup directory
-#foreach ($dir in $sourceDirs) {
-#    if (Test-Path $dir) {
-#        $folderName = Split-Path $dir -Leaf
-#        $destPath = Join-Path $backupDir $folderName
-#        Copy-Item -Path $dir -Destination $destPath -Recurse
-#        Write-Output "Backed up $folderName to $destPath"
-#    } else {
-#        Write-Output "Source directory $dir does not exist."
-#    }
-#}
-#Write-Output "Backup completed to $backupDir"
+    #    "C:\Users\Administrator\FINANCE",
+    #    "C:\Users\Administrator\HR",
+    #    "C:\Users\Administrator\IT",
+    #    "C:\Users\Administrator\Research"
+    #)
+    # Define the root backup directory on the local disk
+    #$backupRootDir = "C:\Backups"
+    # Create a timestamped directory for this backup
+    #$timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+    #$backupDir = Join-Path $backupRootDir ("Backup_" + $timestamp)
+    #New-Item -Path $backupDir -ItemType Directory
+    # Copy each source directory to the timestamped backup directory
+    #foreach ($dir in $sourceDirs) {
+    #    if (Test-Path $dir) {
+    #        $folderName = Split-Path $dir -Leaf
+    #        $destPath = Join-Path $backupDir $folderName
+    #        Copy-Item -Path $dir -Destination $destPath -Recurse
+    #        Write-Output "Backed up $folderName to $destPath"
+    #    } else {
+    #        Write-Output "Source directory $dir does not exist."
+    #    }
+    #}
+    #Write-Output "Backup completed to $backupDir"
 
+
+}
 
 ##################################
 
